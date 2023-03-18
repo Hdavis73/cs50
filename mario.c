6054b9399815448c21b2d69b29242dbb095a2482
Heather <96963524+Hdavis73@users.noreply.github.com>
2023-03-18 03:42:47 +0000

    Fri, Mar 17, 2023, 10:42 PM -05:00

--- mario.c
+++ mario.c
@@ -1,8 +1,17 @@
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    const int num = 3;
+    int num;
+    do
+    {
+        num = get_int("Size:");
+    }
+    while( num < 1);
+
+
+
     for(int i = 0; i < num; i++)
     {
         for(int j = 0; j < num; j++)
