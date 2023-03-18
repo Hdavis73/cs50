9baa388b5a7da1da157ee2a130b36278334b9aa0
Heather <96963524+Hdavis73@users.noreply.github.com>
2023-03-18 20:58:50 +0000

    Sat, Mar 18, 2023, 3:58 PM -05:00

--- problem-set/population.c
+++ problem-set/population.c
@@ -27,25 +27,33 @@ int main(void)
 
     // TODO: Calculate number of years until we reach threshold
 
-    int years_to_end = 0;
+    int i;
 
     int total_llamas = starting_size;
 
-    do
+    for( i = 0; total_llamas < end_size; i++)
     {
         int new_llamas = starting_size / 3;
         int dead_llamas = starting_size / 4;
 
         total_llamas = total_llamas + new_llamas - dead_llamas;
-
-        years_to_end++;
     }
-    while(total_llamas < end_size);
+
+    // do
+    // {
+    //     int new_llamas = starting_size / 3;
+    //     int dead_llamas = starting_size / 4;
+
+    //     total_llamas = total_llamas + new_llamas - dead_llamas;
+
+    //     years_to_end++;
+    // }
+    // while(total_llamas < end_size);
 
 
 
 
     // TODO: Print number of years
 
-    printf("%i\n", years_to_end);
+    printf("%i\n", i);
 }
