792a86e89769bd6226469a4f8b04522fb10cc7c6
Heather <96963524+Hdavis73@users.noreply.github.com>
2023-03-18 20:44:32 +0000

    Sat, Mar 18, 2023, 3:44 PM -05:00

--- problem-set/population.c
+++ problem-set/population.c
@@ -16,13 +16,18 @@ int main(void)
 if(starting_size > 9 && starting_size < end_size)
 {
     int total_llamas = starting_size;
-    int years_to_end;
+    int years_to_end = 0;
 
     do
     {
-        
+        int new_llamas = starting_size / 3;
+        int dead_llamas = starting_size / 4;
+
+        int total_llamas = total_llamas + new_llamas - dead_llamas;
+
+        int years_to_end += 1;
     }
-    while()
+    while(total_llamas < end_size)
 
 }
 
@@ -30,5 +35,5 @@ if(starting_size > 9 && starting_size < end_size)
 
     // TODO: Print number of years
 
-    // printf("%i\n", years_to_size);
+    printf("%i\n", years_to_size);
 }
