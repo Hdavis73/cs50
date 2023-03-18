2e9a67c6c2d89cb48a4630e8ce2345e18b462776
Heather <96963524+Hdavis73@users.noreply.github.com>
2023-03-18 21:45:27 +0000

    Sat, Mar 18, 2023, 4:45 PM -05:00

--- problem-set/mario-more/mario-more.c
+++ problem-set/mario-more/mario-more.c
@@ -3,5 +3,18 @@
 
 int main(void)
 {
-    
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while( height < 1);
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < height; j++)
+        {
+            printf("#  #")
+        }
+    }
 }
