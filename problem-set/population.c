ed8c1e5c85f562995295e4ba3d79a2ec74513327
Heather <96963524+Hdavis73@users.noreply.github.com>
2023-03-18 20:29:41 +0000

    Sat, Mar 18, 2023, 3:29 PM -05:00

---
+++ problem-set/population.c
@@ -0,0 +1,26 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // TODO: Prompt for start size
+
+    int starting_size = get_int("Starting Size: ");
+
+    // TODO: Prompt for end size
+
+    int end_size = get_int("End Size: ");
+
+    // TODO: Calculate number of years until we reach threshold
+
+if(starting_size < 9 || starting_size > end_size)
+{
+    printf("try agrain\n")
+}
+
+
+
+    // TODO: Print number of years
+
+    printf("%i\n", years_to_size);
+}
