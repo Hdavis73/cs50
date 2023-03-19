5abdd775943813f6edf37ecb7e9d6131cea00006
Heather <96963524+Hdavis73@users.noreply.github.com>
2023-03-19 01:17:35 +0000

    Sat, Mar 18, 2023, 8:17 PM -05:00

--- problem-set/credit/credit.c
+++ problem-set/credit/credit.c
@@ -6,9 +6,19 @@ int main(void)
     //get valid input credit card number
     //must be at least 13 characters
     long card_number = get_long("Card Number:");
+    int sum = 0;
 
     //multiply every other digit by 2 starting with second to last digit
-    
+
+    while(card_number > 0)
+    {
+        sum += card_number % 10;
+        card_number = card_number / 10;
+    }
+
+    printf("%i\n", sum);
+
+
     //add the digits in the products together
     //add the sum to the sum of the digits that werent multiplied by two
     //if the last digit of that number is 0 the card is valid
