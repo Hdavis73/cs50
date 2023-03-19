3275a0878e4a3c053ed9d25d4b28b1532ac94548
Heather <96963524+Hdavis73@users.noreply.github.com>
2023-03-19 00:54:12 +0000

    Sat, Mar 18, 2023, 7:54 PM -05:00

--- problem-set/credit/credit.c
+++ problem-set/credit/credit.c
@@ -4,6 +4,9 @@
 int main(void)
 {
     //get valid input credit card number
+    //must be at least 13 characters
+    //multiply every other digit by 2 starting with second to last digit
+    //add the digits in the products together (123456789 8 + 6 + 4 + 2)
 
-    
+
 }
