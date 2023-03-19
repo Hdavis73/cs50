6e6efb3cfafc6443ea2616ab90afc730cb3b0c05
Heather <96963524+Hdavis73@users.noreply.github.com>
2023-03-19 00:01:09 +0000

    Sat, Mar 18, 2023, 7:01 PM -05:00

--- problem-set/mario-more/mario.c
+++ problem-set/mario-more/mario.c
@@ -1,7 +1,35 @@
-#include <cs50.h>
 #include <stdio.h>
+#include <cs50.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int height;
+
+    //get user input, if input is greater than 8
+    do
+    {
+        height = get_int("height:");
+    }
+    while(8 < height || height < 1);
+
+
+    for (int i = height; i >= 0; i--)
+    {
+        for (int h = i; h >= 1; h--){
+            printf(" ");
+        }
+
+        for (int j = i; j <= height; j++){
+            printf("#");
+        }
+
+        printf("  ");
+
+        for (int j = i; j <= height; j++){
+
+            printf("#");
+        }
+
+        printf("\n");
+    }
 }
